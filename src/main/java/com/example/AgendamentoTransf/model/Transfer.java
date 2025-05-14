c4727e02d259f3d78a4377b2e72e0443633e2db8
Bruno Blanquez <b.blanquez@yahoo.com.br>
2025-05-14 08:14:04 -0300

    Adicionando regra de negócio

--- src/main/java/com/example/AgendamentoTransf/model/Transfer.java
+++ src/main/java/com/example/AgendamentoTransf/model/Transfer.java
@@ -81,4 +81,33 @@ public class Transfer {
     public void setDataTransferencia(LocalDate dataTransferencia) {
         this.dataTransferencia = dataTransferencia;
     }
+
+    private BigDecimal calcularTaxa(long dias, BigDecimal valor) {
+        switch (definirIntervaloParaTaxas(dias)) {
+            case 0:
+                return new BigDecimal("3.00").add(valor.multiply(new BigDecimal("0.025")));
+            case 1:
+                return new BigDecimal("12.00");
+            case 2:
+                return valor.multiply(new BigDecimal("0.082"));
+            case 3:
+                return valor.multiply(new BigDecimal("0.069"));
+            case 4:
+                return valor.multiply(new BigDecimal("0.047"));
+            case 5:
+                return valor.multiply(new BigDecimal("0.017"));
+            default:
+                return null;
+        }
+    }
+
+    private int definirIntervaloParaTaxas(long dias) {
+        if (dias == 0) return 0;
+        if (dias <= 10) return 1;
+        if (dias <= 20) return 2;
+        if (dias <= 30) return 3;
+        if (dias <= 40) return 4;
+        if (dias <= 50) return 5;
+        return -1;
+    }
 }
