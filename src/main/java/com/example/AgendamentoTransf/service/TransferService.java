0146f015185ed2a50c4219660011cbc273b6e8c5
Bruno Blanquez <b.blanquez@yahoo.com.br>
2025-05-14 10:57:10 -0300

    Criando controller + refactor service + criando DTO

--- src/main/java/com/example/AgendamentoTransf/service/TransferService.java
+++ src/main/java/com/example/AgendamentoTransf/service/TransferService.java
@@ -1,26 +1,48 @@
 package com.example.AgendamentoTransf.service;
 
+import com.example.AgendamentoTransf.dto.TransferRequest;
+import com.example.AgendamentoTransf.dto.TransferResponse;
 import com.example.AgendamentoTransf.model.Transfer;
 import com.example.AgendamentoTransf.repository.TransferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransferService {
-    private final TransferRepository repository;
 
-    public TransferService(TransferRepository repository) {
-        this.repository = repository;
+    @Autowired
+    private TransferRepository repository;
+
+    public TransferResponse agendar(TransferRequest dto) {
+        LocalDate hoje = LocalDate.now();
+        long dias = ChronoUnit.DAYS.between(LocalDate.now(), dto.getDataTransferencia());
+
+        if (dias > 50) {
+            throw new IllegalArgumentException("Nenhuma taxa aplicável para esse intervalo de dias.");
+        }
+
+        Transfer transfer = new Transfer();
+        transfer.setContaOrigem(dto.getContaOrigem());
+        transfer.setContaDestino(dto.getContaDestino());
+        transfer.setValor(dto.getValor());
+        transfer.setDataAgendamento(hoje);
+        transfer.setDataTransferencia(dto.getDataTransferencia());
+
+        transfer.definirTaxa(dias);
+
+        Transfer novaTransferencia = repository.save(transfer);
+        return new TransferResponse(novaTransferencia);
     }
 
-    public Transfer salvar(Transfer transferencia) {
-        return repository.save(transferencia);
-    }
-
-    public List<Transfer> listarTodas() {
-        return repository.findAll();
+    public List<TransferResponse> listarTodasTransf() {
+        return repository.findAll().stream()
+                .map(TransferResponse::new)
+                .collect(Collectors.toList());
     }
 
 }
